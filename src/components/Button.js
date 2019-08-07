aad8f8811b34fdb42ed9ac3bd5735cccce7fd6c0
njgorton <gorton.nj@gmail.com>
2019-08-06 18:10:45 -0600

    3rd commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import LanguageContext from '../contexts/LanguageContext'
+import ColorContext from '../contexts/ColorContext'
 
+// Commented out code is for the this.context property instead of the Consumer being used below:
 class Button extends React.Component {
-    static contextType = LanguageContext
+    // static contextType = LanguageContext
+    renderSumbit(value) {
+        return value === 'english' ? 'Submit' : 'Voorleggen'
+    }
+
+    renderButton(color) {
+        return (
+            <button className={`ui button ${color}`}>
+                {/* {text} */}
+                <LanguageContext.Consumer>
+                    {(value) => this.renderSumbit(value)}
+                </LanguageContext.Consumer>
+            </button>
+        )
+    }
+
     render() {
-        const text = this.context === 'english' ? 'Submit' : 'Voorleggen'
-        return <button className="ui button primary">{text}</button>
+        // const text = this.context === 'english' ? 'Submit' : 'Voorleggen'
+        return (
+            <ColorContext.Consumer>
+                {(color) => this.renderButton(color)}
+            </ColorContext.Consumer>
+        )
     }
 }
 
